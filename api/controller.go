69a5ee3a2f4a3c6b8c7ab8d188c2db7ec3425d11
lovesaroha <lovesaroha1994@gmail.com>
2021-10-14 17:13:55 +0530

    first commit

--- api/controller.go
+++ api/controller.go
@@ -1,3 +1,8 @@
+/*  Love Saroha
+    lovesaroha1994@gmail.com (email address)
+    https://www.lovesaroha.com (website)
+    https://github.com/lovesaroha  (github)
+*/
 package api
 
 import "net/http"
